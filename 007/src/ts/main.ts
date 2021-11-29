9940524b3901e7997c19cb8ace75bdeb104d4083
naobeep <85398103+naobeep@users.noreply.github.com>
2021-11-29 12:21:57 +0900

    微修正

---
+++ 007/src/ts/main.ts
@@ -0,0 +1,75 @@
+import p5 from 'p5';
+const count = 100;
+const boxes = [];
+let isFire = false;
+
+const sketch = (p: p5) => {
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    // p.createCanvas(600, 400);
+    p.angleMode('degrees');
+    // p.colorMode('hsb');
+    for (let i = 0; i < count; i++) {
+      boxes.push(new Direction());
+    }
+  };
+
+  p.draw = () => {
+    p.background(204);
+    boxes.forEach(box => {
+      isFire && box.update();
+      box.render();
+    });
+
+    // console.log(boxes);
+  };
+
+  p.mousePressed = () => {
+    isFire = true;
+  };
+
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
+
+  class Direction {
+    location: p5.Vector;
+    dLocation: p5.Vector;
+    speed: number;
+    accel: number;
+    constructor() {
+      this.location = p.createVector(p.random(p.width), p.random(p.height));
+      this.dLocation = p.createVector(0, 0);
+      this.speed = 30;
+      this.accel = 1;
+    }
+    update() {
+      const a = p.atan2(p.mouseY - this.location.y, p.mouseX - this.location.x);
+      this.speed -= this.accel;
+      if (this.speed < 0) {
+        this.speed = 0;
+      }
+      this.dLocation = p.createVector(
+        p.cos(a) * -this.speed,
+        p.sin(a) * -this.speed
+      );
+      this.location.add(this.dLocation);
+    }
+    render() {
+      const a = p.atan2(p.mouseY - this.location.y, p.mouseX - this.location.x);
+      p.push();
+      p.translate(this.location.x, this.location.y);
+      p.rotate(a);
+      p.rect(-30, -5, 60, 10);
+      p.pop();
+      if (this.location.x < -100 || this.location.x > p.width + 100) {
+        p.removeElements();
+      }
+      if (this.location.y < -100 || this.location.x > p.height + 100) {
+        p.removeElements();
+      }
+    }
+  }
+};
+
+new p5(sketch);
